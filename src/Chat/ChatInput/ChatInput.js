bf3ea705657168158684b5bea7a3c21b287acd8a
ClarkTheSup <lknkoqq123@g163.com>
2020-09-23 14:58:36 +0800

    [kangning] feat: send message and display on chatbox

--- src/Chat/ChatInput/ChatInput.js
+++ src/Chat/ChatInput/ChatInput.js
@@ -2,11 +2,53 @@ import React, { Component } from 'react';
 import './ChatInput.scss';
 
 class ChatInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      role: '',
+      tags: [],
+    };
+  }
+
+  sendMessage = () => {
+    this.updateText();
+    const message = {
+      ...this.state,
+    };
+    this.props.concatMessage(message);
+    this.clearInput();
+  };
+
+  updateText = () => {
+    const text = document.getElementById('messageText').value;
+    const role = 'CUSTOMER';
+    this.setState({ text, role });
+  };
+
+  hasPressedEnter = (event) => {
+    if (event.keyCode === 13) {
+      return true;
+    }
+    return false;
+  };
+
+  clearInput = () => {
+    document.getElementById('messageText').value = '';
+  };
+
   render() {
     return (
       <footer className="ChatInput">
-        <input type="text" />
-        <button type="button">Send</button>
+        <input
+          type="text"
+          onChange={this.updateText}
+          onKeyUp={(event) => this.hasPressedEnter(event) && this.sendMessage()}
+          id="messageText"
+        />
+        <button type="button" onClick={this.sendMessage}>
+          Send
+        </button>
       </footer>
     );
   }
